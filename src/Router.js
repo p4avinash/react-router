e03ffdade0cbdcb58953e3838149e5136c8a614b
Avinash <p4avinashmaurya@gmail.com>
2024-03-25 14:23:32 +0530

    removed unused event listener

--- src/Router.js
+++ src/Router.js
@@ -3,10 +3,6 @@ import React, { useEffect, useState } from "react"
 export const RouterProvider = ({ router }) => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
-  window.addEventListener("popstate", (e) => {
-    console.log(e.currentTarget)
-  })
-
   useEffect(() => {
     window.addEventListener("navigate", () => {
       console.log("navigate event triggered")
